fb2ec3aae75b9c40799c4bf540f90f3e2eaf0320
Sajjad Ahmad <jogimar14@gmail.com>
2021-01-20 19:18:48 +0500

    use addEventListner to add submit listner to form

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,15 +1,13 @@
-function Book(title, author, pages) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-}
 let library = [];
-function addBook() {
-  let title = document.getElementById('title').value;
-  let author = document.getElementById('author').value;
-  let pages = document.getElementById('pages').value;
-  let book = new Book(title, author, pages);
-  library.push(book);
-  e.preventDefault();
+function Book() {
+  this.title = document.getElementById('title').value;
+  this.author = document.getElementById('author').value;
+  this.pages = document.getElementById('pages').value;
 }
-console.log(library);
+form = document.getElementById('form');
+form.addEventListener('submit', function addBook(event) {
+  let book = new Book();
+  library.push(book);
+  event.preventDefault();
+  document.getElementById("form").reset();
+});
