8496f2ffa2b58438f0f482c3885132af47bebace
Jyoti <jyotikiran12@gmail.com>
2021-01-21 13:36:04 +0530

    Add displayBook method and update addBook method

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,15 +1,39 @@
-function Book(title, author, pages) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
+let myLibrary = [];
+
+//Book constructor
+function Book() {
+  this.title = document.getElementById("title").value;
+  this.author = document.getElementById("author").value;
+  this.pages = document.getElementById("pages").value;
 }
-let library = [];
-function addBook() {
-  let title = document.getElementById('title').value;
-  let author = document.getElementById('author').value;
-  let pages = document.getElementById('pages').value;
-  let book = new Book(title, author, pages);
-  library.push(book);
+
+// Adding book
+function addBookToLibrary(e) {
   e.preventDefault();
+  const book = new Book(title, author, pages);
+  myLibrary.push(book);
+  console.log(myLibrary);
+  displayBook(book);
+  //Clearing the form input data after submit
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("pages").value = "";
 }
-console.log(library);
+
+//Displaying book to the UI
+function displayBook(book) {
+  const list = document.querySelector('#book-list');
+  const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <td class="mx-4">${book.title}</td>
+    <td class="mx-4">${book.author}</td>
+    <td class="mx-4">${book.pages}</td>
+    <td><a href="#" class="btn btn-danger mx-4">Remove</a></td>
+    `
+    list.appendChild(row);
+}
+
+document.getElementById("form").addEventListener('submit', addBookToLibrary);
+
+
