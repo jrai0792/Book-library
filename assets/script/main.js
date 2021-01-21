9d264c5f9e44f686c6a9739132f5028b7314359b
Jyoti <jyotikiran12@gmail.com>
2021-01-21 20:17:22 +0530

    Add deleteBook function

--- assets/script/main.js
+++ assets/script/main.js
@@ -5,6 +5,12 @@ function Book() {
   this.title = document.getElementById("title").value;
   this.author = document.getElementById("author").value;
   this.pages = document.getElementById("pages").value;
+  this.read = (function() {
+    if(document.getElementById('yes').checked)
+      return document.getElementById('yes').value;
+    else
+      return document.getElementById('no').value;
+  }) ();
 }
 
 // Adding book
@@ -26,10 +32,15 @@ function displayBook(book) {
     <td class="mx-4">${book.title}</td>
     <td class="mx-4">${book.author}</td>
     <td class="mx-4">${book.pages}</td>
-    <td><a href="#" class="btn btn-danger mx-4">Remove</a></td>
+    <td class="mx-4">${book.read}</td>
+    <td><a href="#" class="btn btn-danger mx-4" id="statusYes">Yes</a></td>
+    <td><a href="#" class="btn btn-danger" id="statusNo">No</a></td>
+    <td><a href="#" class="btn btn-danger mx-4 delete">Remove</a></td>
     `
     list.appendChild(row);
 }
+
+//Add book form display function
 function showForm(e) {
   document.getElementById('form').style.display = 'block';
 }
@@ -38,4 +49,24 @@ function hideForm(e) {
 }
 document.getElementById('add-book').addEventListener('click', showForm);
 document.getElementById('form').addEventListener('submit', addBookToLibrary);
-document.getElementById('submit-form').addEventListener('click', hideForm);
+document.getElementById('submit-form').addEventListener('submit', hideForm);
+
+//delete a book
+
+function deleteBook(ele) {
+  if(ele.classList.contains("delete")) {
+    ele.parentElement.parentElement.remove();
+  }
+}
+
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  deleteBook(e.target);
+});
+
+//Update read status
+function updateReadBtn(ele) {
+  if(){
+    
+  }
+}
+
