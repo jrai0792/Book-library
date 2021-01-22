78a39911bd6f9992b5fadf45e31c59c1facfc0db
Jyoti <jyotikiran12@gmail.com>
2021-01-22 20:00:24 +0530

    Add changes for loop

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const myLibrary = [];
 
 // Book constructor
@@ -12,55 +13,46 @@ function Book(title, author, pages, read) {
 function displayBook(book) {
   const list = document.querySelector('#book-list');
   const row = document.createElement('tr');
-
   row.innerHTML = `
-    <td class="mx-4">${book.title}</td>
-    <td class="mx-4">${book.author}</td>
-    <td class="mx-4">${book.pages}</td>
-    <td><a href="#" class="btn btn-primary mx-4" id="statusYes">${book.read}</a></td>
-    <td><a href="#" class="btn btn-danger mx-4 delete">Remove</a></td>
-    `;
+      <td class="mx-4">${book.title}</td>
+      <td class="mx-4">${book.author}</td>
+      <td class="mx-4">${book.pages}</td>
+      <td><a href="#" class="btn btn-primary mx-4" id="statusYes">${book.read}</a></td>
+      <td><a href="#" class="btn btn-danger mx-4 delete">Remove</a></td>
+      `;
   list.appendChild(row);
 }
 
 // Adding book
 function addBookToLibrary(e) {
-  let title = document.getElementById('title').value;
-  let author = document.getElementById('author').value;
-  let pages = document.getElementById('pages').value;
-  let read = (function () {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = (function () {
     if (document.getElementById('yes').checked) return document.getElementById('yes').value;
     return document.getElementById('no').value;
   }());
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
-  displayBook(book);
-  e.preventDefault();
 
+  e.preventDefault();
   // Clearing the form input data after submit
   document.getElementById('form').reset();
+  myLibrary.forEach(displayBook(book));
 }
-
 
 // Add book form display function
 function showForm() {
   document.getElementById('form').style.display = 'block';
 }
-let hideform = false;
+
+
 function hideForm() {
-  debugger
-  hideform = !hideform;
-  if(hideform){
-    document.getElementById('formdiv').style.display = 'none';
-  }else
-  {
-    document.getElementById('formdiv').style.display = 'display';
-  }
+  document.getElementById('form').style.display = 'none';
 }
+
 document.getElementById('add-book').addEventListener('click', showForm);
 document.getElementById('form').addEventListener('submit', addBookToLibrary);
-document.getElementById('submit-form').addEventListener('submit', hideForm);
-
 // delete a book
 
 function deleteBook(ele) {
@@ -86,3 +78,4 @@ function changeReadStatus(ele) {
 document.querySelector('#book-list').addEventListener('click', (e) => {
   changeReadStatus(e.target);
 });
+/* eslint-enable no-unused-vars */
